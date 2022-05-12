2a5fc89de21544be4f62b7015eaca4c1a725a181
nosianuri <nurinosia@gmail.com>
2022-05-12 10:23:10 +0600

    seting submit button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { useState } from "react";
 
 function App() {
+  
+  const [isMousedOver, setMouseOver] = useState(false);
+  
   const [name, setName] = useState("");
   const [headingText, setHeading] = useState("");
+  
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
 
   function handleChange(event) {
     console.log(event.target.value);
@@ -25,7 +37,12 @@ function App() {
           placeholder="What's your name?"
           value={name}
         />
-        <button type="submit">Submit</button>
+        <button
+        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+         type="submit">Submit</button>
       </form>
     </div>
   );
